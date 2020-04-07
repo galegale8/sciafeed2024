c1de6b6fffbee70f17ce195db6ab53d5b6563e96
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2020-04-07 11:28:50 +0200

    advanced draft for compute PREC indicators

---
+++ sciafeed/compute.py
@@ -0,0 +1,242 @@
+"""
+This module contains functions and utilities that extracts information from a `data` record
+or from a set of `data` records.
+`data` is a tuple of kind (metadata, datetime object, par_code, par_value, flag) .
+"""
+from datetime import datetime, timedelta
+
+
+def sum_records_by_hour_groups(day_records, hours_interval):
+    """
+    Return a new set of records obtaining sum of values if they belong to the same
+    groups of hours in the day. Each time group lasts `hours_interval` hours.
+    It assume all the records are valid and values are not None and of the same par_code.
+
+    :param day_records: list of records of the same day
+    :param hours_interval: interval in hours of the splitting of a day
+    :return: the new set of records splitted by the hour
+    """
+    if not day_records:
+        return []
+    metadata = day_records[0][0]
+    day = day_records[0][1]
+    par_code = day_records[0][2]
+    start_time = datetime(day.year, day.month, day.day, 0, 0)
+    new_records = []
+    for min_hour in range(0, 24, hours_interval):
+        max_hour = min_hour + hours_interval
+        subrecords = [r for r in day_records if min_hour <= r[1].hour < max_hour]
+        if not subrecords:
+            continue
+        tot = sum([s[3] for s in subrecords])
+        new_time = start_time + timedelta(hours=min_hour)
+        new_record = (metadata, new_time, par_code, tot, True)
+        new_records.append(new_record)
+    return new_records
+
+
+def wet_distribution(input_records):
+    """
+    It returns the tuple (dry, wet_01, wet_02, wet_03, wet_04, wet_05) where:
+    * dry: num of input records with PREC <= 1
+    * wet_01: num of records with PREC in ]1, 5]
+    * wet_02: num of records with PREC in ]5, 10]
+    * wet_03: num of records with PREC in ]10, 20]
+    * wet_04: num of records with PREC in ]20, 50]
+    * wet_05: num of records with PREC > 50
+    It assumes input records are all of PREC, same station, valid and with not null values.
+
+    :param input_records: input records of PREC
+    :return: (dry, wet_01, wet_02, wet_03, wet_04, wet_05)
+    """
+    dry = len([d[3] for d in input_records if d[3] <= 1])
+    wet_01 = len([d[3] for d in input_records if 1 < d[3] <= 5])
+    wet_02 = len([d[3] for d in input_records if 5 < d[3] <= 10])
+    wet_03 = len([d[3] for d in input_records if 10 < d[3] <= 20])
+    wet_04 = len([d[3] for d in input_records if 20 < d[3] <= 50])
+    wet_05 = len([d[3] for d in input_records if d[3] > 50])
+    return dry, wet_01, wet_02, wet_03, wet_04, wet_05
+
+
+def compute_prec24(day_records, at_least_perc=0.75):
+    """
+    Compute "precipitazione cumulata giornaliera". It will fill the field 'prec24' of
+    table 'sciapgg.ds__preci'.
+    `day_records' is a list of `data` objects of PREC for a fixed day and a fixed station.
+    It returns the tuple (flag, val_tot, val_mx, data_mx) where:
+    * flag: (ndati, wht)
+    * val_tot: sum of PREC values on these records
+    * val_mx: max value of PREC
+    * data_mx: datetime of the max value of PREC
+    If num of valid values/24 >= `at_least_perc` --> wht = 1, otherwise 0
+
+    :param day_records: list of `data` objects for a day and a station.
+    :param at_least_perc: minimum percentage of valid data for the validation flag
+    :return: (flag, val_tot, val_mx, data_mx)
+    """
+    if not day_records:
+        return (0, 0), None, None, None
+    valid_values = [r[3] for r in day_records if r[4] and r[3] is not None]
+    ndati = len(valid_values)
+    val_tot = None
+    val_mx = None
+    data_mx = None
+    wht = 0
+    if ndati:
+        val_tot = sum(valid_values)
+        val_mx = max(valid_values)
+        data_mx = [r[1] for r in day_records if r[3] == val_mx][0]
+        wht = ndati / 24 >= at_least_perc and 1 or 0
+    flag = (ndati, wht)
+    return flag, val_tot, val_mx, data_mx
+
+
+def compute_cl_prec24(day_records):
+    """
+    Compute "distribuzione precipitazione cumulata giornaliera".
+    It will fill the field 'cl_prec24' of table 'sciapgg.ds__preci'.
+    `day_records' is a list of `data` objects of PREC for a fixed day and a fixed station.
+    It returns the tuple (dry, wet_01, wet_02, wet_03, wet_04, wet_05) where:
+    * dry: num of records with PREC <= 1
+    * wet_01: num of records with PREC in ]1, 5]
+    * wet_02: num of records with PREC in ]5, 10]
+    * wet_03: num of records with PREC in ]10, 20]
+    * wet_04: num of records with PREC in ]20, 50]
+    * wet_05: num of records with PREC > 50
+
+    :param day_records: list of `data` objects for a day and a station.
+    :return: (dry, wet_01, wet_02, wet_03, wet_04, wet_05)
+    """
+    valid_records = [d for d in day_records if d[4] and d[3] is not None]
+    return wet_distribution(valid_records)
+
+
+def compute_prec01(day_records, at_least_perc=0.75):
+    """
+    Compute "precipitazione cumulata su 1 ora". It will fill the field 'prec01' of
+    table 'sciapgg.ds__preci'.
+    `day_records' is a list of `data` objects of PREC for a fixed day and a fixed station.
+    It returns the tuple (flag, val_mx, data_mx) where:
+    * flag: (ndati, wht)
+    * val_mx: max value of PREC
+    * data_mx: datetime of the max value of PREC
+
+    :param day_records: list of `data` objects for a hour and a station
+    :param at_least_perc: minimum percentage of valid data for the validation flag
+    :return: (flag, val_mx, data_mx)
+    """
+    if not day_records:
+        return (0, 0), None, None
+    valid_records = [r for r in day_records if r[4] and r[3] is not None]
+    if not valid_records:
+        return (0, 0), None, None
+    ndati = len(valid_records)
+    wht = ndati / 24 >= at_least_perc and 1 or 0
+    flag = (ndati, wht)
+    # valid_records = sum_records_by_hour_groups(valid_records, 1)
+    val_mx = max([r[3] for r in valid_records])
+    data_mx = valid_records[valid_records.index(val_mx)][1]
+    return flag, val_mx, data_mx
+
+
+def compute_prec06(day_records, at_least_perc=0.75):
+    """
+    Compute "precipitazione cumulata su 6 ore". It will fill the field 'prec06' of
+    table 'sciapgg.ds__preci'.
+    `day_records' is a list of `data` objects of PREC for a fixed day and a fixed station.
+    It assumes also records are referring to date times (attribute `hour` of index 1 of each
+    day record).
+    It returns the tuple (flag, val_mx, data_mx) where:
+    * flag: (ndati, wht)
+    * val_mx: max value of PREC (cumulated in 4 groups of same time ranges)
+    * data_mx: datetime of the max value of PREC
+
+    :param day_records: list of `data` objects for a day and a station
+    :param at_least_perc: minimum percentage of valid data for the validation flag
+    :return: (flag, val_mx, data_mx)
+    """
+    if not day_records:
+        return (0, 0), None, None
+    valid_records = [r for r in day_records if r[4] and r[3] is not None]
+    if not valid_records:
+        return (0, 0), None, None
+    ndati = len(valid_records)
+    wht = ndati / 24 <= at_least_perc and 0 or 1
+    flag = (ndati, wht)
+    new_records = sum_records_by_hour_groups(valid_records, 6)
+    val_mx = max([r[3] for r in new_records])
+    data_mx = [r[1] for r in new_records if r[3] == val_mx][0]
+    return flag, val_mx, data_mx
+
+
+def compute_cl_prec06(day_records):
+    """
+    Compute "distribuzione precipitazione cumulata su 6 ore".
+    It will fill the field 'cl_prec06' of table 'sciapgg.ds__preci'.
+    `day_records' is a list of `data` objects of PREC for a fixed day and a fixed station.
+    It returns the tuple (dry, wet_01, wet_02, wet_03, wet_04, wet_05) where:
+    * dry: num of records with PREC <= 1
+    * wet_01: num of records with PREC in ]1, 5]
+    * wet_02: num of records with PREC in ]5, 10]
+    * wet_03: num of records with PREC in ]10, 20]
+    * wet_04: num of records with PREC in ]20, 50]
+    * wet_05: num of records with PREC > 50
+
+    :param day_records: list of `data` objects for a day and a station.
+    :return: (dry, wet_01, wet_02, wet_03, wet_04, wet_05)
+    """
+    valid_records = [r for r in day_records if r[4] and r[3] is not None]
+    new_records = sum_records_by_hour_groups(valid_records, 6)
+    return wet_distribution(new_records)
+
+
+def compute_prec12(day_records, at_least_perc=0.75):
+    """
+    Compute "precipitazione cumulata su 12 ore". It will fill the field 'prec12' of
+    table 'sciapgg.ds__preci'.
+    `day_records' is a list of `data` objects of PREC for a fixed day and a fixed station.
+    It assumes also records are referring to date times (attribute `hour` of index 1 of each
+    day record).
+    It returns the tuple (flag, val_mx, data_mx) where:
+    * flag: (ndati, wht)
+    * val_mx: max value of PREC (cumulated in 4 groups of same time ranges)
+    * data_mx: datetime of the max value of PREC
+
+    :param day_records: list of `data` objects for a day and a station
+    :param at_least_perc: minimum percentage of valid data for the validation flag
+    :return: (flag, val_mx, data_mx)
+    """
+    if not day_records:
+        return (0, 0), None, None
+    valid_records = [r for r in day_records if r[4] and r[3] is not None]
+    if not valid_records:
+        return (0, 0), None, None
+    ndati = len(valid_records)
+    wht = ndati / 24 <= at_least_perc and 0 or 1
+    flag = (ndati, wht)
+    new_records = sum_records_by_hour_groups(valid_records, 12)
+    val_mx = max([r[3] for r in new_records])
+    data_mx = [r[1] for r in new_records if r[3] == val_mx][0]
+    return flag, val_mx, data_mx
+
+
+def compute_cl_prec12(day_records):
+    """
+    Compute "distribuzione precipitazione cumulata su 12 ore".
+    It will fill the field 'cl_prec12' of table 'sciapgg.ds__preci'.
+    `day_records' is a list of `data` objects of PREC for a fixed day and a fixed station.
+    It returns the tuple (dry, wet_01, wet_02, wet_03, wet_04, wet_05) where:
+    * dry: num of records with PREC <= 1
+    * wet_01: num of records with PREC in ]1, 5]
+    * wet_02: num of records with PREC in ]5, 10]
+    * wet_03: num of records with PREC in ]10, 20]
+    * wet_04: num of records with PREC in ]20, 50]
+    * wet_05: num of records with PREC > 50
+
+    :param day_records: list of `data` objects for a day and a station.
+    :return: (dry, wet_01, wet_02, wet_03, wet_04, wet_05)
+    """
+    valid_records = [r for r in day_records if r[4] and r[3] is not None]
+    new_records = sum_records_by_hour_groups(valid_records, 12)
+    return wet_distribution(new_records)
+
---
+++ tests/test_62_compute.py
@@ -0,0 +1,252 @@
+
+from datetime import datetime
+
+from sciafeed import compute
+
+from pprint import pprint
+
+sample_metadata = {'a metadata': 'a value'}
+DAILY_PREC_SET = [
+        (sample_metadata, datetime(2020, 1, 1, 0, 0), 'PREC', 0, True),
+        (sample_metadata, datetime(2020, 1, 1, 1, 0), 'PREC', 1, True),
+        (sample_metadata, datetime(2020, 1, 1, 2, 0), 'PREC', 2, True),
+        (sample_metadata, datetime(2020, 1, 1, 3, 0), 'PREC', 3, True),
+        (sample_metadata, datetime(2020, 1, 1, 4, 0), 'PREC', 4, True),
+        (sample_metadata, datetime(2020, 1, 1, 5, 0), 'PREC', 5, True),
+        (sample_metadata, datetime(2020, 1, 1, 6, 0), 'PREC', 6, True),
+        (sample_metadata, datetime(2020, 1, 1, 7, 0), 'PREC', 7, True),
+        (sample_metadata, datetime(2020, 1, 1, 8, 0), 'PREC', 8, True),
+        (sample_metadata, datetime(2020, 1, 1, 9, 0), 'PREC', 9, True),
+        (sample_metadata, datetime(2020, 1, 1, 10, 0), 'PREC', 10, True),
+        (sample_metadata, datetime(2020, 1, 1, 11, 0), 'PREC', 11, True),
+        (sample_metadata, datetime(2020, 1, 1, 12, 0), 'PREC', 12, True),
+        (sample_metadata, datetime(2020, 1, 1, 13, 0), 'PREC', 13, True),
+        (sample_metadata, datetime(2020, 1, 1, 14, 0), 'PREC', 14, True),
+        (sample_metadata, datetime(2020, 1, 1, 15, 0), 'PREC', 15, True),
+        (sample_metadata, datetime(2020, 1, 1, 16, 0), 'PREC', 16, True),
+        (sample_metadata, datetime(2020, 1, 1, 17, 0), 'PREC', 17, True),
+        (sample_metadata, datetime(2020, 1, 1, 18, 0), 'PREC', 18, True),
+        (sample_metadata, datetime(2020, 1, 1, 19, 0), 'PREC', 19, True),
+        (sample_metadata, datetime(2020, 1, 1, 20, 0), 'PREC', 20, True),
+        (sample_metadata, datetime(2020, 1, 1, 21, 0), 'PREC', 21, True),
+        (sample_metadata, datetime(2020, 1, 1, 22, 0), 'PREC', 22, True),
+        (sample_metadata, datetime(2020, 1, 1, 23, 0), 'PREC', 23, True),
+    ]
+
+
+def test_sum_records_by_hour_groups():
+    day_records = DAILY_PREC_SET
+    # step h=1
+    new_records = compute.sum_records_by_hour_groups(day_records, 1)
+    assert new_records == day_records
+
+    # step h=2
+    new_records = compute.sum_records_by_hour_groups(day_records, 2)
+    assert new_records == [
+        (sample_metadata, datetime(2020, 1, 1, 0, 0), 'PREC', 1, True),
+        (sample_metadata, datetime(2020, 1, 1, 2, 0), 'PREC', 5, True),
+        (sample_metadata, datetime(2020, 1, 1, 4, 0), 'PREC', 9, True),
+        (sample_metadata, datetime(2020, 1, 1, 6, 0), 'PREC', 13, True),
+        (sample_metadata, datetime(2020, 1, 1, 8, 0), 'PREC', 17, True),
+        (sample_metadata, datetime(2020, 1, 1, 10, 0), 'PREC', 21, True),
+        (sample_metadata, datetime(2020, 1, 1, 12, 0), 'PREC', 25, True),
+        (sample_metadata, datetime(2020, 1, 1, 14, 0), 'PREC', 29, True),
+        (sample_metadata, datetime(2020, 1, 1, 16, 0), 'PREC', 33, True),
+        (sample_metadata, datetime(2020, 1, 1, 18, 0), 'PREC', 37, True),
+        (sample_metadata, datetime(2020, 1, 1, 20, 0), 'PREC', 41, True),
+        (sample_metadata, datetime(2020, 1, 1, 22, 0), 'PREC', 45, True)
+    ]
+
+    # step h=3
+    new_records = compute.sum_records_by_hour_groups(day_records, 3)
+    assert new_records == [
+        (sample_metadata, datetime(2020, 1, 1, 0, 0), 'PREC', 3, True),
+        (sample_metadata, datetime(2020, 1, 1, 3, 0), 'PREC', 12, True),
+        (sample_metadata, datetime(2020, 1, 1, 6, 0), 'PREC', 21, True),
+        (sample_metadata, datetime(2020, 1, 1, 9, 0), 'PREC', 30, True),
+        (sample_metadata, datetime(2020, 1, 1, 12, 0), 'PREC', 39, True),
+        (sample_metadata, datetime(2020, 1, 1, 15, 0), 'PREC', 48, True),
+        (sample_metadata, datetime(2020, 1, 1, 18, 0), 'PREC', 57, True),
+        (sample_metadata, datetime(2020, 1, 1, 21, 0), 'PREC', 66, True)
+    ]
+
+    # step h=4
+    new_records = compute.sum_records_by_hour_groups(day_records, 4)
+    assert new_records == [
+        (sample_metadata, datetime(2020, 1, 1, 0, 0), 'PREC', 6, True),
+        (sample_metadata, datetime(2020, 1, 1, 4, 0), 'PREC', 22, True),
+        (sample_metadata, datetime(2020, 1, 1, 8, 0), 'PREC', 38, True),
+        (sample_metadata, datetime(2020, 1, 1, 12, 0), 'PREC', 54, True),
+        (sample_metadata, datetime(2020, 1, 1, 16, 0), 'PREC', 70, True),
+        (sample_metadata, datetime(2020, 1, 1, 20, 0), 'PREC', 86, True)
+    ]
+
+    # step h=6
+    new_records = compute.sum_records_by_hour_groups(day_records, 6)
+    assert new_records == [
+        (sample_metadata, datetime(2020, 1, 1, 0, 0), 'PREC', 15, True),
+        (sample_metadata, datetime(2020, 1, 1, 6, 0), 'PREC', 51, True),
+        (sample_metadata, datetime(2020, 1, 1, 12, 0), 'PREC', 87, True),
+        (sample_metadata, datetime(2020, 1, 1, 18, 0), 'PREC', 123, True)
+    ]
+
+    # step h=8
+    new_records = compute.sum_records_by_hour_groups(day_records, 8)
+    assert new_records == [
+        (sample_metadata, datetime(2020, 1, 1, 0, 0), 'PREC', 28, True),
+        (sample_metadata, datetime(2020, 1, 1, 8, 0), 'PREC', 92, True),
+        (sample_metadata, datetime(2020, 1, 1, 16, 0), 'PREC', 156, True)
+    ]
+
+    # step h=12
+    new_records = compute.sum_records_by_hour_groups(day_records, 12)
+    assert new_records == [
+        (sample_metadata, datetime(2020, 1, 1, 0, 0), 'PREC', 66, True),
+        (sample_metadata, datetime(2020, 1, 1, 12, 0), 'PREC', 210, True)
+    ]
+
+    # step h=24
+    new_records = compute.sum_records_by_hour_groups(day_records, 24)
+    assert new_records == [
+        (sample_metadata, datetime(2020, 1, 1, 0, 0), 'PREC', 276, True)
+    ]
+
+
+def test_wet_distribution():
+    day_records = DAILY_PREC_SET
+    res = compute.wet_distribution(day_records)
+    assert res == (2, 4, 5, 10, 3, 0)
+
+
+def test_compute_prec24():
+    # good set
+    day_records = DAILY_PREC_SET
+    flag, val_tot, val_mx, data_mx = compute.compute_prec24(day_records, at_least_perc=0.75)
+    assert (flag, val_tot, val_mx, data_mx) == ((24, 1), 276, 23, datetime(2020, 1, 1, 23, 0))
+
+    # partially good
+    day_records = [
+        (sample_metadata, datetime(2020, 1, 1, 0, 0), 'PREC', 0, False),
+        (sample_metadata, datetime(2020, 1, 1, 1, 0), 'PREC', 1, False),
+        (sample_metadata, datetime(2020, 1, 1, 2, 0), 'PREC', 2, False),
+        (sample_metadata, datetime(2020, 1, 1, 3, 0), 'PREC', 3, False),
+        (sample_metadata, datetime(2020, 1, 1, 4, 0), 'PREC', 4, False),
+        (sample_metadata, datetime(2020, 1, 1, 5, 0), 'PREC', 5, False),
+        (sample_metadata, datetime(2020, 1, 1, 6, 0), 'PREC', 6, True),
+        (sample_metadata, datetime(2020, 1, 1, 7, 0), 'PREC', 7, True),
+        (sample_metadata, datetime(2020, 1, 1, 8, 0), 'PREC', 8, True),
+        (sample_metadata, datetime(2020, 1, 1, 9, 0), 'PREC', 9, True),
+        (sample_metadata, datetime(2020, 1, 1, 10, 0), 'PREC', 10, True),
+        (sample_metadata, datetime(2020, 1, 1, 11, 0), 'PREC', 11, True),
+        (sample_metadata, datetime(2020, 1, 1, 12, 0), 'PREC', 12, True),
+        (sample_metadata, datetime(2020, 1, 1, 13, 0), 'PREC', 13, True),
+        (sample_metadata, datetime(2020, 1, 1, 14, 0), 'PREC', 14, True),
+        (sample_metadata, datetime(2020, 1, 1, 15, 0), 'PREC', 15, True),
+        (sample_metadata, datetime(2020, 1, 1, 16, 0), 'PREC', 16, True),
+        (sample_metadata, datetime(2020, 1, 1, 17, 0), 'PREC', 17, True),
+        (sample_metadata, datetime(2020, 1, 1, 18, 0), 'PREC', 18, True),
+        (sample_metadata, datetime(2020, 1, 1, 19, 0), 'PREC', 19, True),
+        (sample_metadata, datetime(2020, 1, 1, 20, 0), 'PREC', 20, True),
+        (sample_metadata, datetime(2020, 1, 1, 21, 0), 'PREC', 21, True),
+        (sample_metadata, datetime(2020, 1, 1, 22, 0), 'PREC', 22, True),
+        (sample_metadata, datetime(2020, 1, 1, 23, 0), 'PREC', 23, True),
+    ]
+    flag, val_tot, val_mx, data_mx = compute.compute_prec24(day_records, at_least_perc=0.75)
+    assert (flag, val_tot, val_mx, data_mx) == ((18, 1), 261, 23, datetime(2020, 1, 1, 23, 0))
+
+    # partially bad (higher percentage)
+    flag, val_tot, val_mx, data_mx = compute.compute_prec24(day_records, at_least_perc=0.80)
+    assert (flag, val_tot, val_mx, data_mx) == ((18, 0), 261, 23, datetime(2020, 1, 1, 23, 0))
+
+    # good but missing too many
+    day_records = [
+        (sample_metadata, datetime(2020, 1, 1, 0, 0), 'PREC', 0, True),
+        (sample_metadata, datetime(2020, 1, 1, 1, 0), 'PREC', 1, True),
+        (sample_metadata, datetime(2020, 1, 1, 2, 0), 'PREC', 2, True),
+        (sample_metadata, datetime(2020, 1, 1, 3, 0), 'PREC', 3, True),
+        (sample_metadata, datetime(2020, 1, 1, 4, 0), 'PREC', 4, True),
+        (sample_metadata, datetime(2020, 1, 1, 5, 0), 'PREC', 5, True),
+    ]
+    flag, val_tot, val_mx, data_mx = compute.compute_prec24(day_records, at_least_perc=0.80)
+    assert (flag, val_tot, val_mx, data_mx) == ((6, 0), 15, 5, datetime(2020, 1, 1, 5, 0))
+
+    # empty
+    day_records = []
+    flag, val_tot, val_mx, data_mx = compute.compute_prec24(day_records, at_least_perc=0.80)
+    assert (flag, val_tot, val_mx, data_mx) == ((0, 0), None, None, None)
+
+
+def test_compute_cl_prec24():
+    day_records = [
+        (sample_metadata, datetime(2020, 1, 1, 0, 0), 'PREC', 0, False),
+        (sample_metadata, datetime(2020, 1, 1, 1, 0), 'PREC', 1, False),
+        (sample_metadata, datetime(2020, 1, 1, 2, 0), 'PREC', 2, False),
+        (sample_metadata, datetime(2020, 1, 1, 3, 0), 'PREC', 3, False),
+        (sample_metadata, datetime(2020, 1, 1, 4, 0), 'PREC', 4, False),
+        (sample_metadata, datetime(2020, 1, 1, 5, 0), 'PREC', 5, False),
+        (sample_metadata, datetime(2020, 1, 1, 6, 0), 'PREC', 6, True),
+        (sample_metadata, datetime(2020, 1, 1, 7, 0), 'PREC', 7, True),
+        (sample_metadata, datetime(2020, 1, 1, 8, 0), 'PREC', 8, True),
+        (sample_metadata, datetime(2020, 1, 1, 9, 0), 'PREC', 9, True),
+        (sample_metadata, datetime(2020, 1, 1, 10, 0), 'PREC', 10, True),
+        (sample_metadata, datetime(2020, 1, 1, 11, 0), 'PREC', 11, True),
+        (sample_metadata, datetime(2020, 1, 1, 12, 0), 'PREC', 12, True),
+        (sample_metadata, datetime(2020, 1, 1, 13, 0), 'PREC', 13, True),
+        (sample_metadata, datetime(2020, 1, 1, 14, 0), 'PREC', 14, True),
+        (sample_metadata, datetime(2020, 1, 1, 15, 0), 'PREC', 15, True),
+        (sample_metadata, datetime(2020, 1, 1, 16, 0), 'PREC', 16, True),
+        (sample_metadata, datetime(2020, 1, 1, 17, 0), 'PREC', 17, True),
+        (sample_metadata, datetime(2020, 1, 1, 18, 0), 'PREC', 18, True),
+        (sample_metadata, datetime(2020, 1, 1, 19, 0), 'PREC', 19, True),
+        (sample_metadata, datetime(2020, 1, 1, 20, 0), 'PREC', 20, True),
+        (sample_metadata, datetime(2020, 1, 1, 21, 0), 'PREC', 21, True),
+        (sample_metadata, datetime(2020, 1, 1, 22, 0), 'PREC', 22, True),
+        (sample_metadata, datetime(2020, 1, 1, 23, 0), 'PREC', 23, True),
+    ]
+    res = compute.compute_cl_prec24(day_records)
+    assert res == (0, 0, 5, 10, 3, 0)
+
+
+def test_compute_prec01():
+    day_records = DAILY_PREC_SET
+    flag, val_tot, val_mx, data_mx = compute.compute_prec24(day_records, at_least_perc=0.75)
+    assert (flag, val_mx, data_mx) == ((24, 1), 23, datetime(2020, 1, 1, 23, 0))
+
+    # good but missing too many
+    day_records = [
+        (sample_metadata, datetime(2020, 1, 1, 0, 0), 'PREC', 0, True),
+        (sample_metadata, datetime(2020, 1, 1, 1, 0), 'PREC', 1, True),
+        (sample_metadata, datetime(2020, 1, 1, 2, 0), 'PREC', 2, True),
+        (sample_metadata, datetime(2020, 1, 1, 3, 0), 'PREC', 3, True),
+        (sample_metadata, datetime(2020, 1, 1, 4, 0), 'PREC', 4, True),
+        (sample_metadata, datetime(2020, 1, 1, 5, 0), 'PREC', 5, True),
+    ]
+    flag, val_tot, val_mx, data_mx = compute.compute_prec24(day_records, at_least_perc=0.80)
+    assert (flag, val_mx, data_mx) == ((6, 0), 5, datetime(2020, 1, 1, 5, 0))
+    # with low tolherance
+    flag, val_tot, val_mx, data_mx = compute.compute_prec24(day_records, at_least_perc=0.10)
+    assert (flag, val_mx, data_mx) == ((6, 1), 5, datetime(2020, 1, 1, 5, 0))
+
+
+def test_compute_prec06():
+    day_records = DAILY_PREC_SET
+    flag, val_mx, data_mx = compute.compute_prec06(day_records, at_least_perc=0.75)
+    assert (flag, val_mx, data_mx) == ((24, 1), 123, datetime(2020, 1, 1, 18, 0))
+
+
+def test_compute_cl_prec06():
+    day_records = DAILY_PREC_SET
+    dry, wet_01, wet_02, wet_03, wet_04, wet_05 = compute.compute_cl_prec06(day_records)
+    assert (dry, wet_01, wet_02, wet_03, wet_04, wet_05) == (0, 0, 0, 1, 0, 3)
+
+
+def test_compute_prec12():
+    day_records = DAILY_PREC_SET
+    flag, val_mx, data_mx = compute.compute_prec12(day_records, at_least_perc=0.75)
+    assert (flag, val_mx, data_mx) == ((24, 1), 210, datetime(2020, 1, 1, 12, 0))
+
+
+def test_compute_cl_prec12():
+    day_records = DAILY_PREC_SET
+    dry, wet_01, wet_02, wet_03, wet_04, wet_05 = compute.compute_cl_prec12(day_records)
+    assert (dry, wet_01, wet_02, wet_03, wet_04, wet_05) == (0, 0, 0, 0, 0, 2)

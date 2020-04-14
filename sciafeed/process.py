4b06cc113db46fe678aecd25a99272a1cb99c921
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2020-04-14 17:20:02 +0200

    new kind of implementation for make report

--- sciafeed/process.py
+++ sciafeed/process.py
@@ -6,62 +6,7 @@ from sciafeed import export
 from sciafeed import formats
 
 
-def parse_and_check(filepath, parameters_filepath, limiting_params=None, format_label=None):
-    """
-    Read a file located at `filepath`, and parse data inside it, doing
-    - format validation
-    - weak climatologic check
-    - internal consistence check
-    Return the tuple (err_msgs, parsed data) where `err_msgs` is the list of tuples
-    (row index, error message) of the errors found.
-
-    :param filepath: path to the input file
-    :param parameters_filepath: path to the CSV file containing info about stored parameters
-    :param limiting_params: dictionary of limiting parameters for each parameter code
-    :param format_label: the label string (according to `sciafeed.formats.FORMATS`)
-    :return: (err_msgs, data_parsed)
-    """
-    if not format_label:
-        format_label, format_module = formats.guess_format(filepath)
-    else:
-        format_module = dict(formats.FORMATS).get(format_label)
-    if not format_module:
-        return [(0, "the file has unknown format")], None
-    load_parameter_f = getattr(format_module, 'load_parameter_file')
-    load_parameter_thresholds_f = getattr(format_module, 'load_parameter_thresholds')
-    parse_row_f = getattr(format_module, 'parse_row')
-    rows_generator_f = getattr(format_module, 'rows_generator')
-
-    metadata = formats.extract_metadata(filepath, parameters_filepath, format_label)
-    par_map = load_parameter_f(parameters_filepath)
-    par_thresholds = load_parameter_thresholds_f(parameters_filepath)
-
-    err_msgs = []
-    data = []
-    fmt_err_msgs = formats.validate_format(filepath, parameters_filepath, format_label)
-    err_msgs.extend(fmt_err_msgs)
-    fmt_err_indexes_dict = dict(fmt_err_msgs)
-    if 0 in fmt_err_indexes_dict:
-        # global error, no parsing
-        return err_msgs, None
-
-    for i, row in rows_generator_f(filepath, par_map, metadata):
-        if not row or i in fmt_err_indexes_dict:
-            continue
-        metadata['row'] = i
-        row_data = parse_row_f(row, par_map, metadata=metadata)
-        err_msgs1_row, row_data = checks.data_weak_climatologic_check(
-            row_data, par_thresholds)
-        err_msgs2_row, row_data = checks.data_internal_consistence_check(
-            row_data, limiting_params)
-        data.extend(row_data)
-        err_msgs.extend([(i, err_msg1_row[1]) for err_msg1_row in err_msgs1_row])
-        err_msgs.extend([(i, err_msg2_row[1]) for err_msg2_row in err_msgs2_row])
-    ret_value = err_msgs, data
-    return ret_value
-
-
-def make_report(in_filepath, out_filepath=None, outdata_filepath=None,
+def make_report(in_filepath, out_filepath=None, outdata_filepath=None, do_checks=True,
                 parameters_filepath=None, limiting_params=None):
     """
     Read a file located at `in_filepath` and generate a report on the parsing.
@@ -72,6 +17,7 @@ def make_report(in_filepath, out_filepath=None, outdata_filepath=None,
     :param in_filepath: input file
     :param out_filepath: path of the output report
     :param outdata_filepath: path of the output file containing data
+    :param do_checks: True if must do checks, False otherwise
     :param parameters_filepath: path to the CSV file containing info about stored parameters
     :param limiting_params: dictionary of limiting parameters for each parameter code
     :return: (report_strings, data_parsed)
@@ -90,8 +36,18 @@ def make_report(in_filepath, out_filepath=None, outdata_filepath=None,
     msgs.append('=' * len(msg))
     msgs.append('')
 
-    err_msgs, data_parsed = parse_and_check(
-        in_filepath, parameters_filepath=parameters_filepath, limiting_params=limiting_params)
+    parse_f = getattr(format_module, 'parse')
+    load_parameter_thresholds_f = getattr(format_module, 'load_parameter_thresholds')
+    par_thresholds = load_parameter_thresholds_f(parameters_filepath)
+    # 1. parsing
+    data, err_msgs = parse_f(in_filepath, parameters_filepath)
+    if do_checks:
+        # 2. weak climatologic check
+        wcc_err_msgs, data = checks.data_weak_climatologic_check(data, par_thresholds)
+        # 3. internal consistence check
+        icc_err_msgs, data = checks.data_internal_consistence_check(data, limiting_params)
+        err_msgs += wcc_err_msgs + icc_err_msgs
+
     if not err_msgs:
         msg = "No errors found"
         msgs.append(msg)
@@ -101,7 +57,7 @@ def make_report(in_filepath, out_filepath=None, outdata_filepath=None,
 
     if outdata_filepath:
         msgs.append('')
-        export.export2csv(data_parsed, outdata_filepath)
+        export.export2csv(data, outdata_filepath)
         msg = "Data saved on file %r" % outdata_filepath
         msgs.append(msg)
 
@@ -116,4 +72,4 @@ def make_report(in_filepath, out_filepath=None, outdata_filepath=None,
             for msg in msgs:
                 fp.write(msg + '\n')
 
-    return msgs, data_parsed
+    return msgs, data

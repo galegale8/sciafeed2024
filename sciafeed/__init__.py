3c64e4bf237ac8098c502860fd6203b7b2180797
Alessio Siniscalchi <a.siniscalchi@bopen.eu>
2020-11-10 01:32:18 +0100

    work in progress with gui

--- sciafeed/__init__.py
+++ sciafeed/__init__.py
@@ -2,5 +2,6 @@
 from os.path import abspath, dirname, join
 
 this_path = abspath(dirname(__file__))
+DESIGNER_PATH = join(this_path, 'designer')
 TEMPLATES_PATH = join(this_path, 'templates')
 LOG_NAME = 'SCIA-FEED'
--- sciafeed/gui.py
+++ sciafeed/gui.py
@@ -4,41 +4,120 @@ import sys
 
 from PyQt4 import QtGui, QtCore, uic
 
-from sciafeed import this_path
+from sciafeed import DESIGNER_PATH
+from sciafeed.designer.download_hiscentral_window import _fromUtf8, \
+    Ui_Form as Ui_Download_hiscentral_form
+from sciafeed.designer.download_er_window import Ui_Form as Ui_Download_er_form
 
 
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
+class DownloadEr(QtGui.QWidget, Ui_Download_er_form):
+    close_download_er_signal = QtCore.pyqtSignal()
+    run_download_er_signal = QtCore.pyqtSignal()
 
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+    def __init__(self, *args, **kwargs):
+        super(DownloadEr, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+    def setupUi(self, Ui_Download_er_form):
+        super(DownloadEr, self).setupUi(Ui_Download_er_form)
+        # connect all the buttons of the widget to the corresponding functions
+        self.download_er_close.clicked.connect(self.close)
+        self.download_er_run.clicked.connect(self.run_download_er)
+
+    def closeEvent(self, event):
+        self.close_download_er_signal.emit()
+
+    def run_download_er(self):
+        # emit the run signal
+        self.run_download_er_signal.emit()
+
+
+class DownloadHiscentral(QtGui.QMainWindow, Ui_Download_hiscentral_form):
+    close_download_hiscentral_signal = QtCore.pyqtSignal()
+    run_download_hiscentral_signal = QtCore.pyqtSignal()
+
+    def __init__(self, *args, **kwargs):
+        super(DownloadHiscentral, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+    def setupUi(self, Ui_Download_hiscentral_form):
+        super(DownloadHiscentral, self).setupUi(Ui_Download_hiscentral_form)
+        # connect all the buttons of the widget to the corresponding functions
+        self.download_hiscentral_close.clicked.connect(self.close)
+        self.download_hiscentral_run.clicked.connect(self.run_download_hiscentral)
+
+    def closeEvent(self, event):
+        self.close_download_hiscentral_signal.emit()
+
+    def run_download_hiscentral(self):
+        # emit the run signal
+        self.run_download_hiscentral_signal.emit()
 
 
 class SciaFeedGui(QtGui.QMainWindow):
+    open_hiscentral_signal = QtCore.pyqtSignal()
+    open_er_signal = QtCore.pyqtSignal()
+
     def __init__(self):
         super(SciaFeedGui, self).__init__()
-        self.download_hiscentral_window = QtGui.QMainWindow()
-        uic.loadUi(join(this_path, 'designer/sciafeed_main.ui.xml'), self)
-        self.download_hiscentral_window = uic.loadUi(
-            join(this_path, 'designer/download_hiscentral.ui.xml'))
-        self.set_connections()
+        uic.loadUi(join(DESIGNER_PATH, 'sciafeed_main.ui.xml'), self)
+        # connect all the buttons with the corresponding functions for opening the windows
+        self.OpenWindow_download_hiscentral.clicked.connect(self.open_download_hiscentral)
+        self.OpenWindow_download_er.clicked.connect(self.open_download_er)
 
-    def set_connections(self):
-        QtCore.QObject.connect(self.OpenWindow_download_hiscentral,
-                               QtCore.SIGNAL(_fromUtf8("clicked(bool)")),
-                               self.download_hiscentral_window.show)
+    def open_download_hiscentral(self):
+        self.open_hiscentral_signal.emit()
+
+    def open_download_er(self):
+        self.open_er_signal.emit()
+
+
+class Controller:
+    def show_main(self):
+        self.main_window = SciaFeedGui()
+        # collego tutte le emissioni di segnali della main window
+        # alle funzioni corrispondenti show_... del controller
+        # self.main_window.open_er_signal.connect(self.show_download_er)
+        self.main_window.open_hiscentral_signal.connect(self.show_download_hiscentral)
+        self.main_window.open_er_signal.connect(self.show_download_er)
+        self.main_window.show()
+
+    def show_download_hiscentral(self):
+        self.download_hiscentral_window = DownloadHiscentral()
+        # connect all run and close signals of the window to the controller functions
+        self.download_hiscentral_window.close_download_hiscentral_signal.connect(
+            self.close_download_hiscentral)
+        self.download_hiscentral_window.run_download_hiscentral_signal.connect(
+            self.run_download_hiscentral)
+        self.download_hiscentral_window.show()
+
+    def show_download_er(self):
+        self.download_er_window = DownloadEr()
+        # connect all run and close signals of the window to the controller functions
+        self.download_er_window.close_download_er_signal.connect(self.close_download_er)
+        self.download_er_window.run_download_er_signal.connect(self.run_download_er)
+
+        self.download_er_window.show()
+
+    def run_download_hiscentral(self):
+        # execute the script, interacting with the widgets of the window
+        print('run hiscentral!')
+
+    def close_download_hiscentral(self):
+        print('close hiscentral!')
+        self.download_hiscentral_window.close()
+
+    def run_download_er(self):
+        # execute the script, interacting with the widgets of the window
+        print('run er!')
+
+    def close_download_er(self):
+        print('close er!')
+        self.download_er_window.close()
 
 
 def run_sciafeed_gui():
     app = QtGui.QApplication(sys.argv)
-    main_window = SciaFeedGui()
-    main_window.show()
+    controller = Controller()
+    controller.show_main()
     sys.exit(app.exec_())
